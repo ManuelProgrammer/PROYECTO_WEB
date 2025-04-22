1295c1956d2bc8b12de427b075ac14cbe462c06e
Manuel Programmer <fontalvocoronadom9@gmail.com>
2025-04-21 19:58:01 -0500

    ii

--- mi_proyecto/config.php
+++ mi_proyecto/config.php
@@ -1,7 +1,6 @@
 <?php
 $protocol = "https://"; // Siempre HTTPS, Render lo soporta
 $host = $_SERVER['HTTP_HOST'];
-$basePath = "https://proyecto-web-jr8l.onrender.com"; // Asegúrate que coincide con tu estructura real
 
-define('BASE_URL', $protocol . $host . $basePath);
+define('BASE_URL', $protocol . $host); // ❌ sin /mi_proyecto
 ?>
